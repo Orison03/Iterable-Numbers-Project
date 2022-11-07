023df186a93b68f4252c9aac1036db31860267fa
Orison <orisontetteh@gmail.com>
2022-11-07 01:34:14 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const items = [...document.querySelectorAll(".number")]
+// console.log(items);
+
+
+const updateCount = (el) => {
+  const value = parseInt(el.dataset.value)
+//   const increment = Math.ceil(value/1000)
+          //OR
+  const increment = 1
+  let initialValue = 0
+
+  const increaseCount = setInterval(()=>{
+   initialValue += increment
+
+ if(initialValue > value){
+    el.textContent = `${value}+`
+    clearInterval(increaseCount);
+    return
+ }
+   
+   el.textContent = `${initialValue}+`;
+  },1)
+};
+
+items.forEach((item)=>{
+   updateCount(item)
+})
+
